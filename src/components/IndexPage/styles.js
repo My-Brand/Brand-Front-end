7f6127980fad9b2c719db059da62cb1f0d529c7e
Issa Jean Marie <jeanmarieissa@gmail.com>
2021-07-25 12:33:36 +0200

    Changing measure to % unit

--- src/components/IndexPage/styles.js
+++ src/components/IndexPage/styles.js
@@ -18,7 +18,7 @@ export const IndexPageContainer = styled.section`
 
   @media screen and (max-width: 768px) {
     padding: 0 0;
-    height: calc(100vh - 50px);
+    height: calc(100% - 0.6rem);
   }
 `;
 
